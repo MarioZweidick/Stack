be19808e0ede2ee7b10a774f36a97147bfc6ee20
MarioZweidick <mario.zweidick2@gmail.com>
2021-11-06 13:53:28 +0100

    added some logic to the stack class and created and implemented a DemoApp including a main class

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,22 +1,22 @@
 package stack;
 
-public class Stack
+public class Stack<T>
 {
     private int counter;
-    private int[] array;
+    private T[] array;
     /*
     * private --> nur innerhalb der Klasse
     * public --> von überall  aufrufbar
     * nichts -->  package protected. Nur innerhalb des packages*/
 
-    public Stack(int[] array)
+    public Stack(T[] array)
     {
         this.array = array;
         counter = 0;
 
     }
 
-    public void push (int value)
+    public void push(T value)
     {
         if(counter >= array.length)
             System.out.println("Stack is full. It already has " + array.length + " elements!") ;
@@ -25,10 +25,10 @@ public class Stack
         counter++;
     }
 
-    public int pop ()
+    public T pop ()
     {
         if(counter <= 0)
-            return -1;
+            return null;
 
         counter--;
         return array[counter];
--- src/stack/StackDemo.java
+++ src/stack/StackDemo.java
@@ -1,5 +1,25 @@
 package stack;
 
+import java.net.Inet4Address;
+
 public class StackDemo
 {
+    public static void main(String[] args)
+    {
+        Stack<Integer> stapel = new Stack(new Integer[7]);
+
+        stapel.push(7);
+        stapel.push(5);
+        stapel.push(6);
+
+        System.out.println("stapel.pop() =  " + stapel.pop());
+        System.out.println("stapel.pop() =  " + stapel.pop());
+
+        stapel.push(3);
+
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+    }
+
 }
